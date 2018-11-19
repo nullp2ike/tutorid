2d1d583077dc555b9ed589f3a9cb5aebadb727ea
Sander <sander.kivivare@gmail.com>
2018-11-19 23:55:39 +0200

    Add some comments

--- src/test/java/tests/BookingReschedulingChecks.java
+++ src/test/java/tests/BookingReschedulingChecks.java
@@ -23,22 +23,29 @@ public class BookingReschedulingChecks {
         final Tutor tutor = new TutorCreator(Any.tutorUsername()).create();
         final Student student = new StudentCreator(Any.studentUsername()).create();
 
+        //Creates new availability for tutor, tomorrow and 1H in length
         final Response r = new Availability().tomorrowFromNow1H().add(tutor);
         r.then().body("description", equalTo("Availability created"));
 
+        //Creates the request body builder for the booking request
         final BookingRequestBuilder builder = new DefaultBookingData().bookedByStudentViaTimeSlot(tutor, student);
 
+        //Creates the actual request(puts the body inside it using the builder)
+        //Response contains the created booking request ID, which is necessary for confirming the booking
         BookingRequest bookingRequest = new BookingRequest(builder);
         Response bookingResponse = bookingRequest.asStudent(tutor, student);
 
+        //Confirms the booking
         int bookingId = new JSONObject(bookingResponse.getBody().asString()).getJSONArray("data").getJSONObject(0).getInt("id");
         Response bookingConfirmationResponse = bookingRequest.confirm(tutor.getCookieFilter(), tutor.getNickname(), bookingId);
         bookingConfirmationResponse
                 .then()
                 .statusCode(HttpStatus.SC_OK);
 
+        //Similar to the booking request
         final RescheduleBuilder rescheduleBuilder = new RescheduleData().rescheduledAt(tutor, student, 0, -1);
         final Response rescheduleResponse = new BookingRequest().reschedule(tutor.getCookieFilter(), rescheduleBuilder, tutor.getNickname(), bookingId);
+        //Checks if the response is correct, and in this case that the success message equals false
         rescheduleResponse
                 .then()
                 .statusCode(HttpStatus.SC_OK)
