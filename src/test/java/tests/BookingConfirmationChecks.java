f2291a5b1427adf1504363abc774ba85c9498a26
Khaled Charkie <kncharkie@gmail.com>
2018-11-21 16:39:33 +0200

    Khaled (#3)
    * cancellation tests added
    
    * add test for booking outside available hrs
    
    * add more tests
    
    * missed tests

--- src/test/java/tests/BookingConfirmationChecks.java
+++ src/test/java/tests/BookingConfirmationChecks.java
@@ -40,7 +40,7 @@ public class BookingConfirmationChecks {
         r.then().body("description", equalTo("Availability created"));
 
         //Tutor invites student to join his/her network
-        sendInviteAndAcceptByStudent(tutor, student);
+        sendInviteAndAcceptByStudent2(tutor, student);
 
         //Stetup booking request data
         final BookingRequestBuilder builder = new DefaultBookingData().bookedByStudentViaTimeSlot2(tutor, student);
@@ -52,7 +52,7 @@ public class BookingConfirmationChecks {
                .body("description", equalTo("Tutor policy does not allow requesting booking outside availability & booking request is outside tutor's availability or tutor already has a booking during that time"));
     }
 
-    private void sendInviteAndAcceptByStudent(final Tutor tutor, final Student student) {
+    private void sendInviteAndAcceptByStudent2(final Tutor tutor, final Student student) {
         new Invite().send(tutor, student.getEmail());
         final Response invitationLinkResponse = new Invite().invitationLink(student);
         final int invitationId = new JSONObject(invitationLinkResponse.getBody().asString()).getJSONObject("data").getInt("id");
